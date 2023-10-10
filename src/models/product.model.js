8ecc9134db878329108c6f4b2a6a66068f7f0cb1
Anurag <anuragdobariya001@gmail.com>
2023-10-10 14:53:29 +0530

    Product and cart management ( orders )

---
+++ src/models/product.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    product_name:{
+        type:String,
+        trim:true
+    },
+    product_info:{
+        type: String,
+        trim:true
+    },
+    product_price:{
+        type:Number,
+        trim:true
+    },
+    is_active:{
+        type:Boolean,
+        default:true
+    }
+},
+{
+    timestamps : true,
+    versionKey:false
+}
+)
+
+const product = mongoose.model("product" , productSchema);
+module.exports = product;
